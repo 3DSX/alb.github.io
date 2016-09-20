e849e86c0ae1e37716c0bc31ddc1e6de5a005c39
InitialFail <InitialFail@users.noreply.github.com>
2016-09-20 00:15:20 -0700

    Add script to launch Chromium with disabled security Fix paths

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Works on Debian (or Ubuntu's?) Chromium, since it provides the --user-data-dir flag
+# All the other flags after --user-data-dir work on all Chromium or Chrome
+
+chromium --user-data-dir=/tmp/nosecurity_chromium --disable-web-security --incognito "file://$(dirname $(readlink -f $0))/index.html"
